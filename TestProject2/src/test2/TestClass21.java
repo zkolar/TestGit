7ec1a06b79f2f2e5f4f96ceea0d8e12dd8ec4498
Z.Kolar <inetsoft@ck.t-com.hr>
2013-12-29 12:19:38 +0100

    change 4

---
+++ TestProject2/src/test2/TestClass21.java
@@ -0,0 +1,7 @@
+package test2;
+
+public class TestClass21 {
+
+	private String str1;
+	
+}
---
+++ TestProject3/src/test3/TestClass3.java
@@ -0,0 +1,10 @@
+package test3;
+
+public class TestClass3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
